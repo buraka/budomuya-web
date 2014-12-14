e04720f036207f92f73c263d45617ffbb8a0aee9
Burak Alparslan <burak.alparslan@monitise.com>
2014-12-14 16:31:39 +0200

    Refactor and css
    Refactor and add css classes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
-var oxiNewsApp = angular.module('oxiNewsApp', []);
+var oxiNewsApp = angular.module('oxiNewsApp', ["ui.bootstrap"]);
+
+oxiNewsApp.controller("navMainController", function($scope, $http) {
+    $http.get("http://localhost:8080/OXIWebService/getMenu")
+        .success(function(response) {$scope.menu = response;});
+});
 
 oxiNewsApp.controller('NewListController', function ($scope, $http) {
 	$http.get("http://localhost:8080/OXIWebService/getNews")
-  .success(function(response) {$scope.news = response;});
+        .success(function(response) {$scope.news = response;});
+
+});
+
+oxiNewsApp.controller('CarouselCtrl', function ($scope, $http) {
+
+    $scope.myInterval = 5000;
+	$http.get("http://localhost:8080/OXIWebService/getNews")
+        .success(function(response) {
+
+            $scope.news = response;
+
+        });
 
 });
 
 oxiNewsApp.controller("categoryController", function($scope, $http) {
-  $http.get("http://localhost:8080/OXIWebService/getCategories")
-  .success(function(response) {$scope.categories = response;});
+    $http.get("http://localhost:8080/OXIWebService/getCategories")
+        .success(function(response) {$scope.categories = response;});
 });
